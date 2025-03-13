bc903a52070139a86915cf611205b9a04836e857
Adnan0397 <143485467+Adnan0397@users.noreply.github.com>
2025-03-13 18:13:25 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,4 @@
 import requests
 
-url = "https://www.google.com/books/jsapi.js"
-status = requests.get(url)
-
-
+search_query = input("Enter book titel") #burgeren skal skrive titel. 
+url = f"https://openlibrary.org/search.json?q={search_query}" #bruger f" da det gør det nemmer at indsætte variabler i tekststrenge. en f" er en række af karaktere/bogstaver. 
