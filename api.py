6c255128e633e3c4cbf78d44dbffec6ecbfab601
Adnan0397 <143485467+Adnan0397@users.noreply.github.com>
2025-03-13 22:18:31 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 
-search_query = input("Enter book titel") #burgeren skal skrive titel.  
-url = f"https://openlibrary.org/search.json?q={search_query}" #bruger f" da det gør det nemmer at indsætte variabler i tekststrenge. en f" er en række af karaktere/bogstaver. 
+search = input("Enter book titel") #burgeren skal skrive titel.  
+url = f"https://openlibrary.org/search.json?q={search}" #bruger f" da det gør det nemmer at indsætte variabler i tekststrenge. en f" er en række af karaktere/bogstaver. 
 
 #{} er placeholders
 
