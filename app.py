b41218d73bcaf9880ed1630baf6223b7bd35bf2f
Adnan0397 <143485467+Adnan0397@users.noreply.github.com>
2025-03-17 09:51:12 +0100

    api data

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 app = Flask(__name__) #initialiser vores flask
 
 @app.route("/")
-def index ():
+def index():
     return render_template("index.html")
+
+
+@app.route("/search_book", methods=["GET"])
+def search():
+    if request.method == "GET": 
+        bookquery = request.args["bookquery"]
+        url = f"https://openlibrary.org/search.json?q={bookquery}"
+        response= requests.get(url)
+        data = response.json()
+        print(data["docs"][0])
+        return render_template("search_book.html")
+
+#todoo indsæt data fra api og render det på html
+
+
 
 if __name__ == "__main__":
     app.run()
