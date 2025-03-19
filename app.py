83cfb7106e37b28b6288e766668bd1ca01e08e2d
Adnan0397 <143485467+Adnan0397@users.noreply.github.com>
2025-03-20 00:50:01 +0100

    api

--- app.py
+++ app.py
@@ -12,19 +12,31 @@ def search():
         bookquery = request.args.get("bookquery") #vores query data fra input er stored her. 
         url = f"https://openlibrary.org/search.json?q={bookquery}" #bruger f" da det gør det nemmer at indsætte variabler i tekststrenge. en f" er en række af karaktere/bogstaver. 
         #{} er placeholders
+       
         response= requests.get(url) # bruger get til at hente data. 
+    
+ 
+        if response.status_code == 200:
+            data = response.json() # variabel data der gemmer data.
+        else:
+            print(f"failed{response.status_code}")
+        
         data = response.json() # variabel data der gemmer data.
         
         book = data["docs"][0] #første resultat i vores liste. 01234
         title = book.get("title")
         author = book.get("author_name")
         year = book.get("first_publish_year")
-        value = book.get("key",[0])
+        value = book.get("ia")[0]
+
+        print(value, title, author, year)
+
+        return render_template("search_book.html", title=title, author=author, year=year, value=value)
+
+       
+
+
         
-        print(title, author, year, value)
-
-
-        return render_template("search_html")
 
 
 
