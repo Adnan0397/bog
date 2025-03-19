040205fb612ea995b3c6f791fd99b163aeaf3cba
Adnan0397 <143485467+Adnan0397@users.noreply.github.com>
2025-03-19 22:58:28 +0100

    book cover

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, titel 
 
 #todoo lav id til table, find ud hvordan man fetcher fra api. med request. 
 books = [
-    ("The Metamorphasis", "Franz Kafka", 1970),
+    
     ("gurli gris", "James Gunn", 2010 ),
     ("Doppler", "arlend loe", 2020),
     ("Hunger games", "suzanne collins", 2008 )
