b2893847831ec894a78b5ebfa770ae295999cb21
Adnan0397 <143485467+Adnan0397@users.noreply.github.com>
2025-03-19 13:30:46 +0100

    idk

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ def index():
 
 @app.route("/search_book")
 def search():
-        bookquery = request.args.get("bookquery") #vores query data er fra input er stored her. 
+        bookquery = request.args.get("bookquery") #vores query data fra input er stored her. 
         url = f"https://openlibrary.org/search.json?q={bookquery}" #bruger f" da det gør det nemmer at indsætte variabler i tekststrenge. en f" er en række af karaktere/bogstaver. 
         #{} er placeholders
         response= requests.get(url) # bruger get til at hente data. 
         data = response.json() # variabel data der gemmer data.
         
-        book = data["docs"][0] #første resultat i vores liste. !!01234
-        title = book.get("tittle")
+        book = data["docs"][0] #første resultat i vores liste. 01234
+        title = book.get("title")
         author = book.get("author_name")
         year = book.get("first_publish_year")
-        value = book.get("")
+        value = book.get("key",[0])
         
-        #print(data["docs"][0])
+        print(title, author, year, value)
 
 
         return render_template("search_html")
