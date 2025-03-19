03de0c3138b304679f7b55a45fc30aa6d52be550
Adnan0397 <143485467+Adnan0397@users.noreply.github.com>
2025-03-19 08:53:18 +0100

    flask route og api

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/search_book", methods=["GET"])
+@app.route("/search_book")
 def search():
-    if request.method == "GET": 
-        bookquery = request.args["bookquery"]
-        url = f"https://openlibrary.org/search.json?q={bookquery}"
-        response= requests.get(url)
-        data = response.json()
+        bookquery = request.args.get("bookquery") #vores query data er fra input er stored her. 
+        url = f"https://openlibrary.org/search.json?q={bookquery}" #bruger f" da det gør det nemmer at indsætte variabler i tekststrenge. en f" er en række af karaktere/bogstaver. 
+        #{} er placeholders
+        response= requests.get(url) # bruger get til at hente data. 
+        data = response.json() # variabel data der gemmer data.
         print(data["docs"][0])
-        return render_template("search_book.html")
+        return render_template("search_html")
 
 #todoo indsæt data fra api og render det på html
 
