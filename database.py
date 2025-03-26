bcb7defd6eea90b220754974b0b88bf30ff6446c
Adnan0397 <143485467+Adnan0397@users.noreply.github.com>
2025-03-26 17:57:21 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,25 @@
 import sqlite3
 
-connection = sqlite3.connect("books.db")
 
-cursor = connection.cursor()
+def init_db():
+    connection = sqlite3.connect("books.db")
+    cursor = connection.cursor() #bruges til at lave en connection til databasen og hente data. ligesom en mellemmand. cursor objekt
+    cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, titel string, author string, release_year INTEGER, isbn TEXT)") #sql query
 
-#bruges til at lave en connection til databasen og hente data. ligesom en mellemmand. cursor objekt
-cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, titel string, author string, release_year INTEGER, isbn TEXT)") #sql query
+  cursor.execute(sql, book) #udføre kommandoen 
+  connection.commit() #gemmer ændringerne
 
 
 
-sql = "INSERT INTO books (titel, author, release_year) VALUES (?, ?, ?)"
+def add_to_db():
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO books (titel, author, release_year) VALUES (?, ?, ?)
+    ,(title, author, year, isbn))")
+
+
 #vi laver en prepared statement. for ikke at få sql injection.
-for book in books:              # for loop der ittere over vores tabels indhold 
-    cursor.execute(sql, book) #udføre kommandoen 
-    connection.commit() #gemmer ændringerne
+           # for loop der ittere over vores tabels indhold 
+  
 
 
 
